1f69e805562fbb5ce76807995e77e0d4d66c0cfc
Adavi <sadavi@icct.com>
2016-06-24 13:27:03 -0400

    angularized map with directive for step8

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 This is JS Training Kata
 Steps:
 1.Right click on index.html and run it in your browser
-
+2. For each step,the file, index. html should be moved from the folder to the root
+    This is due to googlemapapi being used without a key.
 Kata Steps:
 
 1.	Install NodeJS
@@ -12,6 +13,6 @@ Kata Steps:
 5.	Build Google Map example with textbox and map, so that when you type a city, or address in the textbox you can display the map, run this via http-server locally.
 6.	Create Object Oriented JS version of #5
 7.	Download Jasmine 2and Blanket JS  to test #6 with 100% coverage
-8.	Angularize #5, use controller example, https://docs.angularjs.org/guide/controller 
+8.	Angularize #5, use controller example, https://docs.angularjs.org/guide/controller
 9.	Create custom directive version of #8
 10.	Add local Storage to #9
---
+++ angular-version-directive/googleMap.js
@@ -0,0 +1,49 @@
+var angMaps = angular.module('angMaps',[]);
+
+
+angMaps.controller('googleMapControl', function ($scope){
+        $scope.mapElement = "map";
+        $scope.zoom = 8;
+        $scope.center = {
+              lat: -34.397,
+              lng: 150.644
+            }
+        $scope.address ="Columbus, OH";
+
+        $scope.initMap = function(){
+            $scope.map = new google.maps.Map(document.getElementById($scope.mapElement),{
+            zoom: $scope.zoom,
+            center: $scope.center
+
+          });
+        }
+        $scope.searchAddress = function(){
+              var geocoder = new google.maps.Geocoder();
+              var map = $scope.map;
+
+              var address = $scope.address;
+
+              geocoder.geocode({  'address': address }, function(results, status) {
+
+                  map.setCenter(results[0].geometry.location);
+                      var marker = new google.maps.Marker({
+                      map: map,
+                      position: results[0].geometry.location
+                  });
+
+              });
+
+        }
+
+
+
+
+});
+
+angMaps.directive('displayMap', function(){
+
+  return{
+              
+              templateUrl: "../angular-version-directive/template/googleMapDir.html"
+  };
+});
